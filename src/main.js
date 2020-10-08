7d36f7585a0361d38f6c02b446dde442e7cfabf1
970029315 <970029315@qq.com>
2020-10-08 10:28:03 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+//rem
+import 'amfe-flexible'
+//字体图标
+import './assets/font/iconfont.css'
 
+
+Vue.use(Vant)
 import style from './style/index.less'
 Vue.config.productionTip = false
 
